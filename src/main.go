fba281cb947c61cd4f19c5e9950a61f7b95b41ed
abduganiyev <hasanboy.abduganiyev@udevs.io>
2022-06-02 12:20:14 +0500

    added test files

---
+++ src/main.go
@@ -0,0 +1,11 @@
+package main
+ 
+// function which return "geeks"
+func SayHello(name string) string{
+    return "Hello " + name;
+}
+ 
+// main function of package
+func main() {
+    SayHello("Hasanboy")
+}
